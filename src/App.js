5b3ee8a1802a5ff137422127a5dccf80e2517a86
XSabyr <sabur.xotak@mail.ru>
2021-01-28 12:54:19 +0300

    useState for Errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { useState, useRef } from 'react';
 
+function Error(props) {
+  return (
+    <span className="'error" style={{ color: 'red' }}>
+      {props.text}
+    </span>
+  );
+}
+
 function App() {
   // eslint-disable-next-line
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -8,37 +16,20 @@ function App() {
   const passwordRef = useRef(null);
   const formRef = useRef(null);
 
-  const generateError = (text) => {
-    const error = document.createElement('span');
-    error.className = 'error';
-    error.style.color = 'red';
-    error.innerHTML = text;
-    return error;
-  };
-
-  const removeValidation = () => {
-    const errors = formRef.current.querySelectorAll('.error');
-    for (let i = 0; i < errors.length; i++) {
-      errors[i].remove();
-    }
-  };
-
   function validateAndSend(event) {
     event.preventDefault();
-    removeValidation();
-
-    if (!emailRef.current.value) {
-      emailRef.current.after(generateError('Cannot be blank'));
-      return false;
-    } else if (!re.test(String(emailRef.current.value).toLowerCase())) {
-      emailRef.current.after(generateError('Should be an Email'));
+    if (!re.test(String(emailRef.current.value).toLowerCase())) {
+      setEmailError(true);
       return false;
     }
+    setEmailError(false);
 
     if (!passwordRef.current.value) {
-      passwordRef.current.after(generateError('Cannot be blank'));
+      setPasswordError(true);
       return false;
     }
+
+    setPasswordError(false);
 
     const obj = {
       email: emailRef.current.value,
@@ -47,28 +38,25 @@ function App() {
     console.log(JSON.stringify(obj));
   }
 
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
   return (
     <div>
       <form ref={formRef}>
         <section>
           <label>
             Email:
-            <input
-              type="text"
-              id="email"
-              ref={emailRef}
-            />
+            <input type="text" id="email" ref={emailRef} />
           </label>
+          {emailError && <Error text="Should be an email" />}
         </section>
         <section>
           <label>
             Password:
-            <input
-              type="text"
-              id="password"
-              ref={passwordRef}
-            />
+            <input type="text" id="password" ref={passwordRef} />
           </label>
+          {passwordError && <Error text="Cannot be blank" />}
         </section>
         <button type="submit" onClick={validateAndSend}>
           Отправить
@@ -79,3 +67,28 @@ function App() {
 }
 
 export default App;
+
+// const generateError = (text) => {
+//   const error = document.createElement('span');
+//   error.className = 'error';
+//   error.style.color = 'red';
+//   error.innerHTML = text;
+//   return error;
+// };
+
+// const removeValidation = () => {
+//   const errors = formRef.current.querySelectorAll('.error');
+//   for (let i = 0; i < errors.length; i++) {
+//     errors[i].remove();
+//   }
+// };
+
+// removeValidation();
+
+// if (!emailRef.current.value) {
+//   emailRef.current.after(generateError('Cannot be blank'));
+//   return false;
+// } else
+
+// emailRef.current.after(generateError('Should be an Email'));
+// passwordRef.current.after(generateError('Cannot be blank'));

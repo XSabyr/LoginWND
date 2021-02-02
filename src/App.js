50f30ae9fb04e1a76975b6a5c7a630e0fdaee45d
XSabyr <sabur.xotak@mail.ru>
2021-02-02 12:11:40 +0300

    MUI Added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,77 @@
 import React, { useState, useRef } from 'react';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 function Error(props) {
   return (
-    <span className="'error" style={{ color: 'red' }}>
+    <Typography variant="h5" style={{ color: 'red' }}>
       {props.text}
-    </span>
+    </Typography>
   );
 }
 
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
   // eslint-disable-next-line
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
 
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const formRef = useRef(null);
 
+  const classes = useStyles();
+
   function validateAndSend(event) {
     event.preventDefault();
-    if (!re.test(String(emailRef.current.value).toLowerCase())) {
+    if (!re.test(String(emailValue).toLowerCase())) {
       setEmailError(true);
       return false;
     }
     setEmailError(false);
 
-    if (!passwordRef.current.value) {
+    if (!passwordValue) {
       setPasswordError(true);
       return false;
     }
-
     setPasswordError(false);
 
     const obj = {
-      email: emailRef.current.value,
-      password: passwordRef.current.value,
+      email: emailValue,
+      password: passwordValue,
     };
     console.log(JSON.stringify(obj));
   }
@@ -41,52 +79,89 @@ function App() {
   const [emailError, setEmailError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
 
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
   return (
-    <div>
-      <form ref={formRef} onSubmit={validateAndSend}>
-        <section>
-          <label>
-            Email:
-            <input type="text" id="email" ref={emailRef} />
-          </label>
-          {emailError && <Error text="Should be an email" />}
-        </section>
-        <section>
-          <label>
-            Password:
-            <input type="text" id="password" ref={passwordRef} />
-          </label>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} ref={formRef} onSubmit={validateAndSend} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            ref={emailRef}
+            onChange={(event) => setEmailValue(event.target.value)}
+            autoFocus
+          />
+          {emailError && <Error text="Should be an Email" />}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            ref={passwordRef}
+            onChange={(event) => setPasswordValue(event.target.value)}
+            autoComplete="current-password"
+          />
           {passwordError && <Error text="Cannot be blank" />}
-        </section>
-        <button type="submit">Отправить</button>
-      </form>
-    </div>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
 export default App;
-
-// const generateError = (text) => {
-//   const error = document.createElement('span');
-//   error.className = 'error';
-//   error.style.color = 'red';
-//   error.innerHTML = text;
-//   return error;
-// };
-
-// const removeValidation = () => {
-//   const errors = formRef.current.querySelectorAll('.error');
-//   for (let i = 0; i < errors.length; i++) {
-//     errors[i].remove();
-//   }
-// };
-
-// removeValidation();
-
-// if (!emailRef.current.value) {
-//   emailRef.current.after(generateError('Cannot be blank'));
-//   return false;
-// } else
-
-// emailRef.current.after(generateError('Should be an Email'));
-// passwordRef.current.after(generateError('Cannot be blank'));
